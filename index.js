03b864e8c79bb34fbe9565c8e313774e15570166
YaoFamily <albertyqyao@gmail.com>
2022-04-04 20:39:48 +1000

    New Update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import cheater from './cheater.js';
+//Code We need to run
 document.getElementById("hide").addEventListener("click", hide);
 function hide(){
     cheater.hidebyid("ukraine");
 }
 document.getElementById("header").classList.add("heading");
+
+//Example code (Useless code)
+console.log(cheater.randomNumber(2,10));
